58c6e6b38626fea6d7c89d7492f8070a3ae7264c
cunninghamjp <jpcunningham@protonmail.com>
2020-03-30 09:23:35 -0500

    Updating script to reboot based on 'web-server' tag.

--- reboot.py
+++ reboot.py
@@ -1,26 +1,26 @@
 import boto3
-import re #imported for use with regex
 
-session = boto3.Session(profile_name='<aws profile name>')
-ec2 = session.resource('ec2')
+session = boto3.Session(profile_name='Jim')
+ec2 = session.resource('ec2', region_name='us-east-1')
 
-for instance in ec2.instances.all():
-    ec2_attributes = [instance.placement,instance.tags]
+def instance_rebooter():
 
+  # Regex in boto3 seems to be limited based on documentation I could find. '?' is a placeholder for any single character
+  # while '*' takes care of anything afterwards. Created an instance in my own AWS account named "Web-Server-blah" and the script worked.  
+    filters= [{'Name':'tag:Name','Values':['?eb-Server*']}] 
 
+    instances = ec2.instances.filter(Filters=filters)
 
-# Trying to do a if/then to look for AZ or Web Server regex to initiate the reboot but getting errors.
-# Spent most of my time trying to debug this issue, would love to see where I went wrong so I don't make same mistake again.
-
-#Tested to see if below actions work, confirmed they do by looking through CloudTrail logs.
-for instances in ec2.instances.all():
-    instance.reboot()
-
-
-print(ec2_attributes[1]) # Prints: '[{'Key': 'Name', 'Value': 'web-server-blah'}]' to get name of my EC2
-print(ec2_attributes[0]) # Prints: '{'AvailabilityZone': 'us-east-1a', 'GroupName': '', 'Tenancy': 'default'}' to pull AZ
+    for instance in instances:
+        for tags in instance.tags:
+            if tags["Key"] == 'Name':
+                name = tags["Value"]
+                instance.reboot()
 
 
-# '^web-server.*' to reboot all instances with web-server leading the name
-# 'us-east-[1-3]' to reboot anything in us-east
-# '.*' to reboot all instances
+instance_rebooter()
+
+
+# # '^web-server.*' to reboot all instances with web-server leading the name
+# # 'us-east-[1-3]' to reboot anything in us-east
+# # '.*' to reboot all instances
